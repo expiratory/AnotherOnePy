d2742390c12433f4cca9cc3e3b0ae7f2da214f58
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-16 23:16:52 +0300

    add some stuff for fun

---
+++ main.py
@@ -0,0 +1,24 @@
+# эта штука просто обнуляет 2 и 4 цифру пятизначного числа, ничего интересного
+number = int(input("Введите пятизначное число: "))
+while True:
+    if number > 9999 and number < 100000:
+        number = str(number)
+        second_number = number[1]
+        forth_number = number[3]
+        second_number = str(0)
+        forth_number = str(0)
+        print(number[0] + second_number + number[2] + forth_number + number[4])
+        break
+    elif number < -9999 and number > -100000:
+        number = str(number)
+        second_number = number[2]
+        forth_number = number[4]
+        second_number = str(0)
+        forth_number = str(0)
+        print(number[0] + number[1] + second_number + number[3] + forth_number + number[5])
+        break
+    else:
+        while True:
+            number = int(input("Введите ПЯТИЗНАЧНОЕ ЧИСЛО: "))
+            if (number > 9999 and number < 100000) or (number < -9999 and number > -100000):
+                break
